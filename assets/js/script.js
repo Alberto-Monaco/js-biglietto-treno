b1650ca31e00063f59df158b1bc053bbdc5cafa0
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-01 10:12:06 +0200

    assign let price km

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //*--------------problema---------------------------------------------------------------------------------
 // Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// il prezzo del biglietto è definito in base ai km (price_km € al km)
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
@@ -13,8 +13,10 @@ let eta
 let total
 let total_around
 let message
+let price_km
 
 //seconda fase: raccolta dati
+price_km = 0.21
 kilometer = prompt('dimmi il numero di km che vuoi fare')
 eta = prompt('dimmi quanti anni hai')
 kilometer = parseInt(kilometer)
@@ -22,13 +24,13 @@ eta = parseInt(eta)
 //terza fase: elaborazione
 
 if (eta < 18) {
-	total = kilometer * 0.21 - (kilometer * 0.21 * 20) / 100
+	total = kilometer * price_km - (kilometer * price_km * 20) / 100
 	message = 'comprensivo del 20% di sconto'
 } else if (eta >= 65) {
-	total = kilometer * 0.21 - (kilometer * 0.21 * 40) / 100
+	total = kilometer * price_km - (kilometer * price_km * 40) / 100
 	message = 'comprensivo del 40% di sconto'
 } else {
-	total = kilometer * 0.21
+	total = kilometer * price_km
 	message = 'e non hai diritto a sconti'
 }
 
